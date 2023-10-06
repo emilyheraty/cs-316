b3b4a24e5b9e61663502b41ed64177f515f8e5e9
Emily Heraty <ekh18@duke.edu>
2023-10-05 20:27:29 -0400

    first

--- db/load.sql
+++ db/load.sql
@@ -14,3 +14,9 @@ SELECT pg_catalog.setval('public.products_id_seq',
 SELECT pg_catalog.setval('public.purchases_id_seq',
                          (SELECT MAX(id)+1 FROM Purchases),
                          false);
+
+\COPY Feedback FROM 'Feedback.csv' WITH DELIMITER ',' NULL '' CSV
+SELECT pg_catalog.setval('public.feedback_id_seq',
+                         (SELECT MAX(id)+1 FROM Feedback),
+                         false);
+
