4dfeceebbb4cbd02c24ce69c6781fb9b764a8852
Catherine Mbata <cmm199@duke.edu>
2023-12-08 16:19:21 -0500

    add search field

--- app/purchases.py
+++ app/purchases.py
@@ -1,15 +1,22 @@
 from flask import render_template, redirect, Blueprint, request
+from flask_wtf import FlaskForm
 from flask_login import current_user
 from flask_paginate import Pagination, get_page_args
+from wtforms import SearchField, SubmitField
 from .models.order import Order
 from .models.purchase import Purchase
 from .models.inventory import Inventory
+from .models.product import Product
 from flask_paginate import Pagination, get_page_parameter
 from flask import Blueprint
 
 bp = Blueprint('purchases', __name__)
 
 import pandas as pd
+
+class SearchForm(FlaskForm):
+    keyword = SearchField("Find a product")
+    submit = SubmitField("Search")
 
 @bp.route('/purchases', methods = ['GET'])
 def purchases():
@@ -20,10 +27,17 @@ def purchases():
     if current_user.is_authenticated:
         purchases = Purchase.get_all_by_uid(
             current_user.id)
+        products = []
+        for purchase in purchases:
+            products.append(Product.get(purchase.pid))
+
         isseller = Inventory.isSeller(current_user.id)[0][0]
     else:
         purchases = None
+        products = None
         isseller = 0
+
+    searchForm = SearchForm()
 
     chart_data = Purchase.get_chart_data(current_user.id)
     chart_data = pd.DataFrame(chart_data, columns=['time_purchased', 'total_amount', 'number_of_items'])
@@ -36,9 +50,13 @@ def purchases():
     page, per_page, offset = get_page_args(page_parameter='page', per_page_parameter='per_page')
     total = len(purchases)
     pagination_purchases = purchases[offset: offset + per_page]
+    pagination_products = products[offset: offset + per_page]
     pagination = Pagination(page=page, per_page=per_page, total=total)
     return render_template('purchases.html',
-                            purchase_history=pagination_purchases, pagination=pagination, isseller=isseller, years=years, grouped_data=grouped_data.head().to_dict())
+                            purchase_history=pagination_purchases, pagination=pagination, isseller=isseller,
+                            years=years, grouped_data=grouped_data.head().to_dict(), search=search, products=pagination_products,
+                            search_msg="Find a product", searchForm=searchForm)
+
 
 @bp.route('/orders/', methods = ['GET'])
 def orders():
